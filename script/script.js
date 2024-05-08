a8baa9850f8ecd18bc31ff5d1330c7c7a3005d26
seanbuckle <seanlbuckle@gmail.com>
2024-05-08 13:40:32 +0100

    chore: Refactor form submission and message generation functionality

--- script/script.js
+++ script/script.js
@@ -1,33 +1,45 @@
 "use strict";
 window.addEventListener("load", () => {
-    const form = document.getElementById("form");
-    const message = document.getElementById("message");
+    let form = document.getElementById("form");
+    let message = document.getElementById("message");
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        const ratings = {
-            one: document.getElementById("one").checked,
-            two: document.getElementById("two").checked,
-            three: document.getElementById("three").checked,
-            four: document.getElementById("four").checked,
-            five: document.getElementById("five").checked,
-        };
-        const rating = Object.keys(ratings).find(key => ratings[key]) || "";
-        const ratingNumber = rating !== "" ? parseInt(rating) : "";
-        if (ratingNumber === "") {
+        let one = document.querySelector("#one:checked");
+        let two = document.querySelector("#two:checked");
+        let three = document.querySelector("#three:checked");
+        let four = document.querySelector("#four:checked");
+        let five = document.querySelector("#five:checked");
+        let rating;
+        if (one) {
+            rating = 1;
+        }
+        else if (two) {
+            rating = 2;
+        }
+        else if (three) {
+            rating = 3;
+        }
+        else if (four) {
+            rating = 4;
+        }
+        else if (five) {
+            rating = 5;
+        }
+        else {
+            rating = "";
+        }
+        if (rating == "") {
             return false;
         }
         else {
-            message.innerHTML = generateMessage(ratingNumber);
+            message.innerHTML = `
+            <section class="card__message">
+            <img class="card__thank-img" src="./images/illustration-thank-you.svg" alt="Heart icon" />
+            <span class="card__rating">You selected ${rating} out of 5</span>
+            <h2>Thank you!</h2>
+            <p class="card__message-txt">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+            </section>
+            `;
         }
     });
 });
-function generateMessage(rating) {
-    return `
-        <section class="card__message">
-        <img class="card__thank-img" src="./images/illustration-thank-you.svg" alt="Heart icon" />
-        <span class="card__rating">You selected ${rating} out of 5</span>
-        <h2>Thank you!</h2>
-        <p class="card__message-txt">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
-        </section>
-    `;
-}
