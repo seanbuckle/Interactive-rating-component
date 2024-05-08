1f7176a4bdadfb37c72ece697c6fb06b07ad7577
seanbuckle <seanlbuckle@gmail.com>
2024-05-08 13:52:27 +0100

    refactor: Improve form submission and message generation functionality

--- script/script.js
+++ script/script.js
@@ -1,34 +1,18 @@
 "use strict";
 window.addEventListener("load", () => {
-    let form = document.getElementById("form");
-    let message = document.getElementById("message");
+    const form = document.getElementById("form");
+    const message = document.getElementById("message");
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        let one = document.querySelector("#one:checked");
-        let two = document.querySelector("#two:checked");
-        let three = document.querySelector("#three:checked");
-        let four = document.querySelector("#four:checked");
-        let five = document.querySelector("#five:checked");
+        const one = document.getElementById("one")?.checked;
+        const two = document.getElementById("two")?.checked;
+        const three = document.getElementById("three")
+            ?.checked;
+        const four = document.getElementById("four")?.checked;
+        const five = document.getElementById("five")?.checked;
         let rating;
-        if (one) {
-            rating = 1;
-        }
-        else if (two) {
-            rating = 2;
-        }
-        else if (three) {
-            rating = 3;
-        }
-        else if (four) {
-            rating = 4;
-        }
-        else if (five) {
-            rating = 5;
-        }
-        else {
-            rating = "";
-        }
-        if (rating == "") {
+        rating = one ? 1 : two ? 2 : three ? 3 : four ? 4 : five ? 5 : "";
+        if (rating === "") {
             return false;
         }
         else {
@@ -39,7 +23,7 @@ window.addEventListener("load", () => {
             <h2>Thank you!</h2>
             <p class="card__message-txt">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
             </section>
-            `;
+        `;
         }
     });
 });
